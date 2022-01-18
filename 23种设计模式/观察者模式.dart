510c19a4284c119f1422af21223677e8f499f2fa
Tecode <283731869@qq.com>
2022-01-18 21:07:45 +0800

    Event 完善

--- 23种设计模式/观察者模式.dart
+++ 23种设计模式/观察者模式.dart
@@ -1,39 +1,46 @@
 // Observer 责任链
 // 假设这样一个场景：F1赛车到达维修点队友需要马上开始进行换胎、加油、给驾驶员水、
+
 abstract class Observer {
-  void actionWakeUp();
+  void actionWakeUp(Event event);
+}
+
+class Event {
+  late String status;
+  Event(this.status);
 }
 
 class TirePersonnel implements Observer {
-  void changeTire() {
-    print("更换轮胎");
+  void changeTire(Event event) {
+    print("${event.status}->更换轮胎");
   }
 
   @override
-  void actionWakeUp() => changeTire();
+  void actionWakeUp(event) => changeTire(event);
 }
 
 class OilPersonnel implements Observer {
-  void refuel() {
-    print("加入燃油");
+  void refuel(Event event) {
+    print("${event.status}->加入燃油");
   }
 
   @override
-  void actionWakeUp() => refuel();
+  void actionWakeUp(event) => refuel(event);
 }
 
 class Assistant implements Observer {
-  void water() {
-    print("送给驾驶员水");
+  void water(Event event) {
+    print("${event.status}->送给驾驶员水");
   }
 
   @override
-  void actionWakeUp() => water();
+  void actionWakeUp(event) => water(event);
 }
 
 void main() {
   var observers = List.from([OilPersonnel(), Assistant(), TirePersonnel()]);
+  var event = Event("车辆到站");
   for (Observer observer in observers) {
-    observer.actionWakeUp();
+    observer.actionWakeUp(event);
   }
 }
