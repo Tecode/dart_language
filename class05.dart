b18276e3f91333cf1dc37e8de1dcd5999ca81469
Tecode <283731869@qq.com>
2022-01-30 23:10:24 +0800

    super方法

--- class05.dart
+++ class05.dart
@@ -1,8 +1,22 @@
 void main(List<String> args) {
   var book = Books.one();
   print(book.hashCode);
-}
+  var math = MathLerning.one();
+  math.name();
+  }
 
 class Books {
   Books.one();
+  void name() {
+    print("书籍");
+  }
+}
+
+class MathLerning extends Books {
+  MathLerning.one() : super.one();
+  @override
+  void name() {
+    super.name();
+    print("数学学习");
+  }
 }
