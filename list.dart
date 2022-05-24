27aefec1a00a5adf196d28ce1479265f44f7a823
Tecode <283731869@qq.com>
2022-05-24 21:57:35 +0800

    数组截取

--- list.dart
+++ list.dart
@@ -1,6 +1,7 @@
 void main() {
   List<num> list = [1, 2 , 3];
-  List<String> array = ['dog', 'cat'];
+  List<String> array = ['dog', 'cat', 'monkey', 'animal'];
   print(list[2]*list[1]);
+  print(array.sublist(1));
   print(array);
 }
--- main.dart
+++ main.dart
@@ -1,12 +1,7 @@
-import 'dart:convert';
-
 void main() {
-  Map<String, String> value = Map<String, String>();
-  value["userName"] = "澜景之家快捷酒店";
-  value["sessionId"] = "41999fecf6d447cdae7a0d34c0a2ec4e";
-  var base64 = base64Encode(utf8.encode(json.encode(value)));
-  print(base64);
-  print(utf8.decode(base64Decode(base64)));
-  print("之前使用的方法：${String.fromCharCodes(base64Decode("5r6c5pmv5LmL5a625b+r5o236YWS5bqX"))}");
-  print("现在使用的方法：${utf8.decode(base64Decode("5r6c5pmv5LmL5a625b+r5o236YWS5bqX"))}");
+  List<num> list = [1, 2 , 3];
+  List<String> array = ['dog', 'cat', 'monkey', 'animal'];
+  print(list[2]*list[1]);
+  print(array.sublist(1));
+  print(array);
 }
