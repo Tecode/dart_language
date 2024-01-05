d4350737a59c6c7fced8de7972291cc556f84f36
Tecode <283731869@qq.com>
2024-01-05 14:33:56 +0800

    feat:文件读取

--- main.dart
+++ main.dart
@@ -1,7 +1,38 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:async';
+
+Future<File> get localFile async {
+  final path = './assets';
+  return File('$path/toutiaoList.json');
+}
+
+Future<File> writeCounter(counter) async {
+  final file = await localFile;
+  return file.writeAsString('$counter');
+}
+
 void main() {
-  List<num> list = [1, 2 , 3];
-  List<String> array = ['dog', 'cat', 'monkey', 'animal'];
-  print(list[2]*list[1]);
-  print(array.sublist(1));
-  print(array);
+  int startTime = DateTime.now().millisecond;
+
+  // localFile.then((File file) {
+  //   file.writeAsString('${jsonEncode(json)}').then((value) {
+  //     print('$value-------------------?');
+  //   });
+  // });
+  localFile.then((File file) {
+    file.readAsString().then((value) {
+      var jsonData = jsonDecode(value);
+      Map<String, String> cacheData = {};
+      var listData = jsonData['data'] as List<dynamic>;
+      listData.forEach((element) {
+        cacheData.addAll({
+          "title": element['Abstract'],
+          "articleUrl":element['article_url'],
+        });
+      });
+      print("$cacheData \n");
+    });
+    print(DateTime.now().millisecond - startTime);
+  });
 }
